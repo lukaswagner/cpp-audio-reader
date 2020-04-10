c1a44099ddbd101e83eb834628bbfd79ee646cac
Lukas Wagner <lukas.wagner@hpi.de>
2020-04-10 23:23:10 +0200

    add portaudio

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
+#include <portaudio.h>
+
 int main(int argc, char const *argv[])
 {
-    std::cout << "test" << std::endl;
+    const auto info = Pa_GetVersionInfo();
+    std::cout << "Using " << info->versionText << std::endl;
     return 0;
 }
